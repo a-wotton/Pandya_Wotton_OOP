d20044d9c5dacb47fbf3e8a2190f85758cd49dc2
a-wotton <a_wotton@fanshaweonline.ca>
2024-03-09 14:49:29 -0500

    setup project and set up constructors

---
+++ js/foods.js
@@ -0,0 +1,50 @@
+class SingleFood {
+    // Parameters:
+    constructor(
+        name,
+        category,
+        grams,
+        calories,
+        proteins,
+        carbohydrates,
+        sugars,
+        saturatedFat,
+        transFat,
+        image
+    // Properties:
+    ) {
+        this.name = name;
+        this.category = category;
+        this.grams = grams;
+        this.calories = calories;
+        this.proteins = proteins;
+        this.carbohydrates = carbohydrates;
+        this.sugars = sugars;
+        this.fats = {
+            saturated: saturatedFat,
+            trans: transFat
+        } 
+        this.image = image;
+
+    // Methods:
+    }
+}
+
+// class MultiFood extends SingleFood {
+//     constructor (
+//         name,
+//         calories,
+//         proteins,
+//         carbohydrates,
+//         sugars,
+//         saturatedFat,
+//         transFat,
+//         image,
+//         grams
+//     ) {
+//         super(name, calories, proteins, carbohydrates, sugars, saturatedFat, transFat, image)
+//         this.grams = grams
+//     }
+// }
+
+export {SingleFood}
